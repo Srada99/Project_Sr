5aaa6226e8d197449d5f59e40d47ee6f19c8133a
Srada99 <sjrada0@misena.edu.co>
2024-09-18 17:16:12 -0500

    Corregir error  ortográfico en README 2

--- obj/Debug/net8.0/Project_Sr.AssemblyInfo.cs
+++ obj/Debug/net8.0/Project_Sr.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project_Sr")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d051cf0ff03c215441fd601a1d4de405f523e3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project_Sr")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project_Sr")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
